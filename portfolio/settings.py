cd115de12a31355e861f0f76726584c195e1c5cd
JaluDwija37 <jaludwija37@gmail.com>
2023-11-17 07:48:43 +0700

    corsing

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -163,5 +163,5 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://127.0.0.1:5500",
-    "https://jaludwija37.pythonanywhere.com/",
+    "https://jaludwija37.pythonanywhere.com",
 ]
